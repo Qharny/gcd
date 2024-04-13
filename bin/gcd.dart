f068e23b0b0e8ad6588bb3fac17d9107c2fcd7c6
Qharny <cannycc1020@gmail.com>
2024-04-13 10:03:07 +0000

    modified

--- bin/gcd.dart
+++ bin/gcd.dart
@@ -1,5 +1,5 @@
 import 'package:gcd/gcd.dart' as gcd;
 
 void main(List<String> arguments) {
-  print('Hello world: ${gcd.calculate()}!');
+  print('Hello world: ${gcd.solution()}!');
 }
--- lib/gcd.dart
+++ lib/gcd.dart
@@ -1,3 +1,46 @@
-int calculate() {
-  return 6 * 7;
+// a dart program to get greatest common divisor
+
+//  EuclideanAlgorithm
+
+import 'dart:io';
+
+int euclideanAlgorithm(int a, int b) {
+  while (b!= 0) {
+    int temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+
+solution() {
+  int? number1;
+  int? number2;
+
+  while (number1 == null) {
+    try {
+      print("Enter the first number: ");
+      number1 = int.tryParse(stdin.readLineSync()?? '');
+      if (number1 == null) {
+        print("Invalid input. Please enter an integer.");
+      }
+    } catch (e) {
+      print("Integer only!");
+    }
+  }
+
+  while (number2 == null) {
+    try {
+      print("Enter a second number: ");
+      number2 = int.tryParse(stdin.readLineSync()?? '');
+      if (number2 == null) {
+        print("Invalid input. Please enter an integer.");
+      }
+    } catch (e) {
+      print("Integer only!");
+    }
+  }
+
+  int gcd = euclideanAlgorithm(number1, number2);
+  print("The greatest common divisor of $number1 and $number2 is $gcd");
 }
